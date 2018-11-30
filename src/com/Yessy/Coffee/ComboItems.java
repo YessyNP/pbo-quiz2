0d337d9b06ab63590743323221696b70d3f33380
YessyNP <yessynindi@gmail.com>
2018-11-30 18:42:06 +0700

    Class untuk memasukkan jenis Items

---
+++ src/com/Yessy/Coffee/ComboItems.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Yessy.Coffee;
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ *
+ * @author YESSYNP
+ */
+public class ComboItems {
+private HashMap<String , Float> itemses = new HashMap<>() ;
+    
+    public ComboItems() {
+        this.itemses.put("Gula" , new Float(30000)) ;
+        this.itemses.put("Kopi" , new Float(10000)) ;
+        this.itemses.put("Susu" , new Float(20000)) ;
+        this.itemses.put("Es Teh" , new Float(15000)) ;
+        this.itemses.put("Matcha" , new Float(19000)) ;
+        
+    }
+    
+    public ArrayList <String> getSemuaNama () {
+        ArrayList<String> str = new ArrayList<> () ;
+        for (String item : this.itemses.keySet()) {
+            str.add(item) ;
+        }
+        return str ;
+    }
+    
+    public ArrayList <Float> getSemuaHarga() {
+        ArrayList<Float> flt = new ArrayList<> () ;
+        for (float item : this.itemses.values()) {
+            flt.add(item) ;
+        }
+        return flt ;
+    }
+    
+    public void addItem(String nama , float harga) {
+        this.itemses.put(nama , harga) ;
+    }
+}
