33f9f48c16d731720a22d2428a202dee5b1f95f5
YessyNP <yessynindi@gmail.com>
2018-11-30 23:16:00 +0700

    CMengedit Class Transaksi

--- src/com/Yessy/Coffee/Transaksi.java
+++ src/com/Yessy/Coffee/Transaksi.java
@@ -15,22 +15,25 @@ public class Transaksi {
     // variable untuk menampilkan kode pembayaran jenis minuman 
     private final String kode ;
     // variable untuk menampilkan item apa saja yang dibeli
-    private ArrayList<Items> itemses = new ArrayList<>();
+    private ArrayList<Items> items = new ArrayList<>();
     // variable total untuk menampilkan total pembelian minuman
     private float total ; 
     
     // Konstruktor
-    public Transaksi (String kode , ArrayList<Items> itemses){
+    public Transaksi(){
+        this.kode = null;
+    }
+    public Transaksi (String kode , ArrayList<Items> items){
         this.kode = kode ;
-        this.itemses = itemses;
+        this.items = items;
         
     }
         
     // total setter
     public void setTotal () {
         float total = 0 ;
-        for (Items itemses : this.itemses){
-            total += itemses.getTotal();
+        for (Items items : this.items){
+            total += items.getTotal();
         }
         this.total = total ;
     }
@@ -41,8 +44,8 @@ public class Transaksi {
         String obt = "" ;
         obt += "Kode\t\t : " + this.kode +"\n" ;
         obt += "Daftar Belanja : \n" ;
-        for (Items itemses:this.itemses){
-           obt += "\t" + itemses.getNama() + "(x" + itemses.getJumlah() + ") : " + itemses.getTotal() + "\n" ;
+        for (Items items:this.items){
+           obt += "\t" + items.getNama() + "(x" + items.getJumlah() + ") : " + items.getTotal() + "\n" ;
         }
         obt += "Total\t\t : " + this.total ;
         return obt ;
